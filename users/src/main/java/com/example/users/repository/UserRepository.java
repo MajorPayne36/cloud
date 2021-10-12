06c1e242190c2440743af1644e3a5eee86f466cd
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-10-12 22:26:48 +0300

    Update UserRepository.java
    fixed #3

--- users/src/main/java/com/example/users/repository/UserRepository.java
+++ users/src/main/java/com/example/users/repository/UserRepository.java
@@ -10,20 +10,7 @@ import java.util.List;
 
 @Repository
 @Transactional
-//@RequiredArgsConstructor
 public class UserRepository {
-//public interface UserRepository extends JpaRepository<User, String> {
-//    @Query("SELECT e FROM users e WHERE e.id = :id")
-//    Optional<User> findById(@Param("id") long id);
-//
-//    @Query("select u from users u order by u.id desc", )
-//    List<User> getAll();
-
-//    @Modifying
-//    @Transactional
-//    @Query("UPDATE UserEntity e SET e.disabled = :disabled WHERE e.id = :id")
-//    void disableById(@Param("id") long id, @Param("disabled") boolean disabled);
-
 
     @PersistenceContext
     private EntityManager entityManager;
