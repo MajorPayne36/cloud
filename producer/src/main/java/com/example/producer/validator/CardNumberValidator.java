bd9b5fabc07a42ad5684e72f2a4f15add21b7438
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-10-12 22:55:05 +0300

    Update CardNumberValidator.java
    FixEd #5
    Now pattern matches all line

--- producer/src/main/java/com/example/producer/validator/CardNumberValidator.java
+++ producer/src/main/java/com/example/producer/validator/CardNumberValidator.java
@@ -6,6 +6,6 @@ import javax.validation.ConstraintValidatorContext;
 public class CardNumberValidator implements ConstraintValidator<CardNumber, String> {
     @Override
     public boolean isValid(String string, ConstraintValidatorContext constraintValidatorContext) {
-        return !string.isEmpty() && string.trim().matches("[0-9]{16}");
+        return !string.isEmpty() && string.trim().matches("^[0-9]{16}$");
     }
 }
